3aba1abd2b2a6dd260e1cf15147802475c24eacc
unknown <sophearintang@gmail.com>
2018-06-28 01:27:34 +0900

    commit for deployment

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,4 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
-
+require File.expand_path('../initializers/debug_require', __FILE__)
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
