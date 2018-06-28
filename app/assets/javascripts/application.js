4aeb3075512d2597ea8fdc71d94535f7c57ac120
unknown <sophearintang@gmail.com>
2018-06-28 10:28:08 +0900

    precompiling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,12 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require bootstrap
-//= require bootstrap-datepicker
-//= require bootstrap2-toggle
 //= require jquery
 //= require jquery_ujs
 //= require rails-ujs
+//= require bootstrap
+//= require bootstrap-datepicker
+//= require bootstrap2-toggle
 //= require activestorage
 //= require turbolinks
 //= require_tree .
